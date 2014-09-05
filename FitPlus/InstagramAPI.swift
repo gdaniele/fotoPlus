61e286c0012182684ab9321221eeb8b27c7475d9
Giancarlo Daniele <giancarlo@forza.local>
2014-09-05 00:20:08 -0700

    added api model for organization

---
+++ FitPlus/InstagramAPI.swift
@@ -0,0 +1,14 @@
+//
+//  InstagramAPI.swift
+//  FitPlus
+//
+//  Created by Giancarlo Daniele on 9/5/14.
+//  Copyright (c) 2014 Giancarlo Daniele. All rights reserved.
+//
+
+import UIKit
+
+class InstagramAPI: NSObject {
+//   MARK: JSON helpers
+    
+}
